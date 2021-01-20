7a395e5c9e18c9eca916e4018fcb2a486fcd491a
Surya Prakash <surya@scrollstack.com>
2021-01-20 17:46:57 +0530

    tagList Fixes

--- src/components/common/list/list.component.js
+++ src/components/common/list/list.component.js
@@ -1,38 +1,42 @@
 import { useState } from 'react';
 import ListItem from './list-item.component';
 import Details from '../../details/details.component';
-import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Link, Redirect, Switch } from 'react-router-dom';
 
 export default function List() {
     const [list, setList] = useState([]);
 
     import('../../../data/data-structures.mjs').then((data) => {
         if (data?.default?.length) setList(data.default);
-    }).catch((error) => {
-        console.error(error);
-    });
+    }).catch(error => console.error(error))
 
     return (
         <Router>
-            <div className="d-flex justify-content-center">
-                {list.map((item) => {
-                    return (
-                        <Link
-                            key={item.key}
-                            to={`/details?q=${item.key}`}
-                            className="text-decoration-none"
-                        >
-                            <ListItem listItem={item} />
-                        </Link>
-                    );
-                })}
-            </div>
-            <Route
-                path="/details"
-                render={props => (
-                    <Details allItems={list} />
-                )}
-            />
+            <TagList list={list} />
+
+            <Switch>
+                <Route exact path='/' render={() => <Redirect to='/details?q=linked-list' />} />
+                <Route path="/details" render={() => <Details allItems={list} />} />
+            </Switch>
         </Router>
     );
+}
+
+function TagList(props) {
+    const { list } = props
+    return (
+        <div className="d-flex justify-content-center">
+            {list.map((item) => {
+                return (
+                    <Link
+                        key={item.key}
+                        to={`/details?q=${item.key}`}
+                        className="text-decoration-none"
+                    >
+                        <ListItem listItem={item} />
+                    </Link>
+                );
+            })}
+        </div>
+    )
 }
