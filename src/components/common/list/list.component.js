6da2976b0ed656b48c0b3aa58b09b3d339ac87ce
Surya Prakash <surya@scrollstack.com>
2021-01-20 16:30:18 +0530

    code cleanup

--- src/components/common/list/list.component.js
+++ src/components/common/list/list.component.js
@@ -1,36 +1,38 @@
 import { useState } from 'react';
-import {ListItem} from './list-item.component';
+import ListItem from './list-item.component';
 import Details from '../../details/details.component';
-import {Route, BrowserRouter as Router, Link} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
 
-export function List() {
+export default function List() {
     const [list, setList] = useState([]);
 
     import('../../../data/data-structures.mjs').then((data) => {
-        if(data && data.default && data.default.length){
-            setList(data.default);
-        }
-    })
-    .catch((error) => {
+        if (data?.default?.length) setList(data.default);
+    }).catch((error) => {
         console.error(error);
     });
 
     return (
         <Router>
-        <div className="d-flex justify-content-center">
-            {list.map((item) => {
-                return (
-                    <Link className="text-decoration-none" key={item.key} to={`/details?q=${item.key}`}>
-                        <ListItem listItem={item}></ListItem>
-                    </Link>
-                );
-            })}
-        </div>
-            <Route path="/details" render={(props) => (
-    <Details allItems={list} />
-  )}></Route>
+            <div className="d-flex justify-content-center">
+                {list.map((item) => {
+                    return (
+                        <Link
+                            key={item.key}
+                            to={`/details?q=${item.key}`}
+                            className="text-decoration-none"
+                        >
+                            <ListItem listItem={item} />
+                        </Link>
+                    );
+                })}
+            </div>
+            <Route
+                path="/details"
+                render={props => (
+                    <Details allItems={list} />
+                )}
+            />
         </Router>
     );
 }
-
-export default List;
